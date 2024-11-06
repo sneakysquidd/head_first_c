9cc1aa2d99c626ee332783769b59bf47673490b0
Gavin Thomas <gsct2002@gmail.com>
2024-11-06 13:22:49 -0700

    added more pointer examples involving arrays

--- c2/pointers.c
+++ c2/pointers.c
@@ -6,3 +6,18 @@
 //      printf("x is stored at %p\n", &x);
 //      return 0;
 //  }
+
+// int main() {
+//     char s[] = "Hello World";
+//     char *t = s;
+//
+//     printf("%i\n", sizeof(t)); shows difference between sizeof on pointer vs array var
+//     printf("%i\n", sizeof(s));
+// }
+
+int main() {
+    // in general drinks[i] and *(drinks + 1) are equivalent
+    int drinks[] = {4,2,3};
+    printf("1st order: %i drinks\n", drinks[2]);
+    printf("1st order: %i drinks\n", *(drinks+2));
+}
